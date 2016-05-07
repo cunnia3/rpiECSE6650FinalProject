14f184bca751702f37d13147b299ca9cf27ee615
cunnia3 <andrew64ce@gmail.com>
2016-05-07 16:48:20 -0400

    cleaned up code

--- laserReconstruct.py
+++ laserReconstruct.py
@@ -18,18 +18,21 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 # Used to find the red laser line on an image
-def findLine(imageIn):
-    
+# sample rate is the number of pixels to skip over in selecting a new pixel
+def findLine(imageIn, sampleRate = 1, plotStuff = False):
     #print np.size(imageIn,0)
     im=Picture.fromarray(imageIn)
     threshold = 50
-    plt.figure()
-    plt.imshow(im, cmap=plt.cm.gray)
     r, g, b = im.split()
     mask = r.point(lambda i: i>threshold and 255)
-    #mask.show()
-    plt.figure()
-    plt.imshow(mask, cmap=plt.cm.gray)
+    
+    # Optional plot
+    if plotStuff:    
+        plt.figure()
+        plt.imshow(im, cmap=plt.cm.gray)
+        plt.figure()
+        plt.imshow(mask, cmap=plt.cm.gray)
+        
     numCols,numRows = mask.size
     data = np.asarray(mask)
     
@@ -37,11 +40,18 @@ def findLine(imageIn):
     for i in range(0,numRows-1):
         for j in range(0,numCols-1):
             if data[i,j] > 250:
-                #print j,i
                 resultsList.append((j,i))
 
-    return resultsList
+    # Down sample for less points to plot
+    downSampledResults = []
+    for i in range(len(resultsList)):         
+        if i%sampleRate == 0:
+            downSampledResults.append(resultsList[i])
 
+    return downSampledResults
+
+# image_converter handles getting an image from a ROS topic and converting it
+# into an array type that find_line can use
 class image_converter:
 
     def __init__(self):
@@ -68,7 +78,8 @@ class image_converter:
         
 # Given the inrinsic parameters of a camera and the arm it is attached to
 # find the projcetion matrix
-def getPFull(W, armKin):
+# default W is for left camera frame
+def getPFull(armKin, W = np.array([[406.00431,0,590.6386],[0,406.0743,420.9455],[0,0,1]])):
     
         # return (normal, rPos) where normal is the normal of the plane and
     # rPos is a point on the plane ( position of the end effector )
@@ -88,7 +99,6 @@ def getPFull(W, armKin):
     
 def activeStereo3Dfrom2D( p2D, P, planeA, planeB, planeC, planeD):
     #activeStereo3Dfrom2D reconstruct 3D points from 2D image point generated
-
     # Pixel Coordinates
     c = p2D[0]
     r = p2D[1]
@@ -101,17 +111,11 @@ def activeStereo3Dfrom2D( p2D, P, planeA, planeB, planeC, planeD):
     b = np.array([[P[0,2]*planeD/planeC+P[0,3]],[P[1,2]*planeD/planeC+P[1,3]],[P[2,2]*planeD/planeC+P[2,3]]])
     b = b.reshape(3,1)
     # Solve for X,Y and lambda
-    x = np.linalg.inv(A)*b
-    
-    #print "P", [P[2,2]*planeD/planeC+P[2,3]]
-    #print "plane params", planeA, planeB, planeC, planeD
-    #print "shape x ", np.shape(x),"shape A ", np.shape(A), "shape b ", np.shape(b)      
-    
+    x = np.linalg.inv(A)*b   
     
     pX = x[0] # X COORD
     pY = x[1] # Y COORD
     pZ = (planeD-planeA*pX - planeB*pY)/planeC
-    #print "COORDINATES ",pX," ",pY," ",pZ
     return np.array([[pX],[pY],[pZ]])
     
 
@@ -124,8 +128,6 @@ def getPlaneParams(armKin):
     rPos = armKin.forward_position_kinematics()[0:3]
     rOrientation = armKin.forward_position_kinematics()[3:7]
     
-    #print "rPos", rPos
-    #print "rOrientation", rOrientation
     myQuat = Quat(rOrientation)
     R_0R = myQuat._quat2transform()
     
@@ -134,50 +136,13 @@ def getPlaneParams(armKin):
     # and the transform tree
     ex = np.array([[1],[0],[0]])
     normal = np.dot(R_0R.T, ex)
-    print normal
-    return (normal, rPos)
+    planeA = normal[0]
+    planeB = normal[1]
+    planeC= normal[2]
+    planeD = np.dot(normal.reshape(1,3),rPos)
+    return (planeA,planeB,planeC, planeD)
 
-def main():
-    rospy.init_node('baxter_laser_reconstruction')
-    kinR = baxter_kinematics('right')
-    kinL = baxter_kinematics('left')
-    
-    ic = image_converter()
-    
-    
-    start = time.time()
-        
-    # analyze images for 10 seconds
-    detected2DPoints = []
-    reconstructed3D = []
-    while time.time() - start < 1:
-        if ic.newImage: # wait till we have a new image to do anything
-            detected2DPoints = findLine(ic.getImage())
-         
-            # get plane parameters
-            testParams = getPlaneParams(kinR)
-            planeA = testParams[0][0]
-            planeB = testParams[0][1]
-            planeC= testParams[0][2]
-            planeD = np.dot(testParams[0].reshape(1,3),testParams[1])
-            
-            # get projection matrix, camera is on left hand
-            Wleft = np.array([[406.00431,0,590.6386],[0,406.0743,420.9455],[0,0,1]])
-            P = getPFull(Wleft,kinL)
-    
-            # Down sample for less points to plot
-            new2D = []
-            for i in range(len(detected2DPoints)):         
-                if i%20 == 0:
-                    print detected2DPoints[i]
-                    new2D.append(detected2DPoints[i])
-                
-            
-            for detected2DPoint in new2D: 
-                new3D = activeStereo3Dfrom2D(detected2DPoint,P,planeA,planeB,planeC,planeD)
-                reconstructed3D.append(new3D)
-        
-    #print reconstructed3D
+def plot3DResults(reconstructed3D):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     
@@ -188,12 +153,38 @@ def main():
         x.append(point[0])
         y.append(point[1])
         z.append(point[2])
-    plt.figure()
+        
     ax.scatter(x, y, z, c='r', marker='o')
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
     ax.set_zlabel('Z Label')
     plt.show()    
+    
+    
+def main():
+    rospy.init_node('baxter_laser_reconstruction')
+    kinR = baxter_kinematics('right')
+    kinL = baxter_kinematics('left')
+    ic = image_converter()
+    
+    start = time.time()    
+    # analyze images for 10 seconds
+    reconstructed3D = []
+    while time.time() - start < 1:
+        if ic.newImage: # wait till we have a new image to do anything
+            detected2DPoints = findLine(ic.getImage(), sampleRate = 4, plotStuff=True)
+            # get plane parameters
+            planeA,planeB,planeC,planeD = getPlaneParams(kinR)
+            
+            # get projection matrix, camera is on left hand
+            P = getPFull(kinL)           
+            
+            for detected2DPoint in detected2DPoints: 
+                new3D = activeStereo3Dfrom2D(detected2DPoint,P,planeA,planeB,planeC,planeD)
+                reconstructed3D.append(new3D)
+        
+    plot3DResults(reconstructed3D)
+
     
 if __name__ == "__main__":
     main()
